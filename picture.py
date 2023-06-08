77b8c7bd9ba0a2a82970a1485c79e2a02fa3eb11
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-07 23:09:33 -0500

    espejo horizontal y negative

--- picture.py
+++ picture.py
@@ -19,3 +19,32 @@ class Picture:
     vertical = []
     vertical = self.img[::-1]
     return Picture(vertical)
+
+  def horizontalMirror(self):
+    """ Devuelve el espejo horizontal de la imagen """
+    length = len(self.img[0])
+    newimg = []
+
+    for r in self.img:
+      row = ""
+      x = 0
+      while x < length:
+        row += r[length -1 -x]
+        x += 1
+      newimg.append(row)
+
+    return Picture(newimg)
+ # usamos la funcion anterior para invertir color y obtener el negativo
+  def negative(self):
+    """ Devuelve un negativo de la imagen """
+    newimg = []
+
+    for r in self.img:
+      x = 0
+      row = ""
+      while x < len(r):
+        row += self._invColor(r[x])
+        x += 1
+      newimg.append(row)
+
+    return Picture(newimg)
