6cce66798be9ff1ba30c37227c892c473f420cea
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-08 00:17:20 -0500

    Ejercicio 6

---
+++ Ejercicio2.6.py
@@ -0,0 +1,20 @@
+from interpreter import draw
+from chessPictures import *
+
+# Crear la base del tablero (un cuadro negro seguido de un cuadro blanco)
+base = cuadrado.join(cuadrado.negative())
+# Su negativo es lo contrario
+negativeBase = base.negative()
+
+# Una fila estara compuesta por 4 veces la base
+row = base.horizontalRepeat(4)
+negativeRow = negativeBase.horizontalRepeat(4)
+
+# Combinar las filas en pares, la fila original encima y la negativa abajo
+rowPair = row.up(negativeRow)
+
+# Figura compuesta por las filas repetidas verticalmente
+figure = rowPair.verticalRepeat(2)
+
+# Dibujar la figura
+draw(figure)
