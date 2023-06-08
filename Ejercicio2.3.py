9a636bb1add9d56a97cc7af954f2f33da01f9b8b
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-07 23:35:19 -0500

    ejercicio 3 sobre la reina

---
+++ Ejercicio2.3.py
@@ -0,0 +1,8 @@
+from interpreter import draw
+from chessPictures import *
+
+# Crear una figura compuesta por la repetición horizontal de la imagen de la reina 4 veces
+figure = laReina.horizontalRepeat(4)
+
+# Dibujamos
+draw(figure)
