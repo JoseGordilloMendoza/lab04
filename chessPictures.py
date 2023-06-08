7e9e27ae813b1cd9c08f9d298ada83e52c36be11
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-07 23:03:19 -0500

    Variables que representan las piezas

---
+++ chessPictures.py
@@ -0,0 +1,18 @@
+# Importar los módulos "pieces" y "picture"
+from pieces import *
+from picture import *
+
+# Crear un objeto "Picture" para representar las piezas y asignarlo a su variable
+elAlfil = Picture(BISHOP)
+
+elRey = Picture(KING)
+
+elCaballo = Picture(KNIGHT)
+
+elPeon = Picture(PAWN)
+
+laReina = Picture(QUEEN)
+
+laTorre = Picture(ROCK)
+
+cuadrado = Picture(SQUARE)
