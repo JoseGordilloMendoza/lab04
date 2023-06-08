cf96cbc44be00b3a52a84ac997822af3ff7e2813
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-07 23:15:05 -0500

    ejercicio 01, usando al caballo

---
+++ Ejercicio2.1.py
@@ -0,0 +1,14 @@
+from interpreter import draw
+from chessPictures import *
+
+# Crear la primera línea como la unión del caballo y su negativo a la derecha
+line1 = elCaballo.join(elCaballo.negative())
+
+# Crear la segunda línea como la unión del negativo del caballo y el caballo original
+line2 = elCaballo.negative().join(elCaballo)
+
+# Combinar las dos líneas
+figure = line1.up(line2)
+
+# Dibujado
+draw(figure)
